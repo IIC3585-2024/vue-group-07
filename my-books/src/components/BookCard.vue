9d23b1a87fe578a57d26017a12f423a3b713e221
Pedro Palma <pedro.palma@uc.cl>
2024-05-30 01:09:24 -0400

    feat: Add BookCard component

---
+++ my-books/src/components/BookCard.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from 'vue';
+
+const props = defineProps({
+    title: String,
+    author: String
+});
+</script>
+
+<template>
+    <div class="card">
+        <h1>{{ title }}</h1>
+        <p>{{ author }}</p>
+    </div>
+</template>
--- my-books/src/components/Gallery.vue
+++ my-books/src/components/Gallery.vue
@@ -1,5 +1,13 @@
+<script setup>
+import { ref } from 'vue';
+import BookCard from './BookCard.vue';
+
+const books = ref([])
+</script>
+
 <template>
     <div>
         <h2> Gallery </h2>
+        <BookCard v-for="book in books" :id="book.id" :title="book.title" :author="book.author"/>
     </div>
 </template>
