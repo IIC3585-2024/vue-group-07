ba1ab9740f886194aac8c46696fe449b1ff86551
Pedro Palma <pedro.palma@uc.cl>
2024-05-30 11:49:26 -0400

    feat: Add styles to BookCard

--- my-books/src/components/BookCard.vue
+++ my-books/src/components/BookCard.vue
@@ -1,15 +1,33 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 
 const props = defineProps({
     title: String,
-    author: String
+    authors: Array
 });
+
+const authorsString = computed(() => {
+    return props.authors.join(', ').trimEnd();
+});
+
 </script>
 
 <template>
     <div class="card">
-        <h1>{{ title }}</h1>
-        <p>{{ author }}</p>
+        <h2>{{ title }}</h2>
+        <p>{{ authorsString }}</p>
     </div>
 </template>
+
+<style>
+.card {
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    padding: 10px;
+    margin: 10px;
+    width: 16rem;
+    height: 20rem;
+    text-align: center;
+}
+
+</style>
