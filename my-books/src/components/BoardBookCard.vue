e04ae28be476c76b3faf04c320f98c9d0aa05371
Jimnora <djimnez@uc.cl>
2024-06-01 22:42:31 -0400

    feat: create BoardBookCard

---
+++ my-books/src/components/BoardBookCard.vue
@@ -0,0 +1,55 @@
+<script setup>
+import { ref, computed } from 'vue';
+import { useBooksStore } from '@/stores/db';
+
+const props = defineProps({
+    id: Number,
+    title: String,
+    authors: Array,
+    coverI: Number
+});
+
+const authorsString = computed(() => {
+    return props.authors.join(', ').trimEnd();
+});
+
+const coverURL = computed(() => {
+    return `http://covers.openlibrary.org/b/id/${props.coverI}-M.jpg`;
+});
+
+const booksStore = useBooksStore();
+
+const deleteBook = () => {
+    booksStore.deleteBook(props.id);
+}
+
+</script>
+
+<template>
+    <div class="card">
+        <img :src="coverURL" alt="book" class="cover"/>
+        <h2>{{ title }}</h2>
+        <p>{{ authorsString }}</p>
+        <div>
+            <button @click="deleteBook"> Delete </button>
+        </div>
+    </div>
+</template>
+
+<style>
+.card {
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    padding: 10px;
+    margin: 10px;
+    width: 18rem;
+    height: 24rem;
+    text-align: center;
+}
+
+.cover {
+    /* max height of 260px */
+    max-height: 16rem;
+}
+
+</style>
