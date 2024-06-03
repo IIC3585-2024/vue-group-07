63810129c23838bb7537a20fbd0063618b86c04f
Jimnora <djimnez@uc.cl>
2024-06-02 21:17:20 -0400

    fix: correct routing for navBar and books in home

--- my-books/src/components/CategoryBoard.vue
+++ my-books/src/components/CategoryBoard.vue
@@ -7,7 +7,7 @@
                         {{ category.name }}
                     </a>
                 </h3>
-                <BoardBookCard v-for="book in books[category.id]" :title="book.title" :authors="book.author_name" :cover-i="book.cover_i" :id="book.id"/>
+                <BoardBookCard v-for="book in books[category.id]" :title="book.title" :authors="book.author_name" :cover-i="book.cover_i" :id="book.id" @click="moveToBook(book)"/>
             </div>
         </div>
     </div>
@@ -69,9 +69,15 @@ export default {
             store.setCurrentCategory(category);
             router.push({ name: 'gallery', params: { category: category.id } });
         }
+        function moveToBook(book) {
+            const currentStore = useCurrentStore();
+            currentStore.setCurrentBook(book);
+            router.push({ name: 'book' , params: { id: book.id } });
+        }
 
         return {
-            moveCategory
+            moveCategory,
+            moveToBook
         }
     },
 
